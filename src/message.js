f6edfe04da239921faa975c2c5963646641b4248
JacobThomas <bjacobt@gmail.com>
2013-02-01 17:37:56 -0800

    added search by number and delete functions

--- src/message.js
+++ src/message.js
@@ -3,6 +3,9 @@ var message = {};
 //place holder for developer's callback function for getMessages and getMessage method
 var getMessagesCallback;
 
+//place holder for developer's callback function for search by number
+var searchByNumberCallback;
+
 var messageServiceUrl = "https://api.tfoundry.com/a1/messages/messages/";
 
 sendMessageSuccess = function(data, textStatus, jqXHR) {
@@ -25,6 +28,28 @@ getMessagesError = function(data, textStatus, jqXHR) {
 	var returnData = {};
 	returnData.textStatus = textStatus;
 	getMessagesCallback(JSON.stringify(returnData));
+};
+
+deleteMessageSuccess = function(data, textStatus, jqXHR) {
+	console.log("success deleteMessage. textStatus = "+textStatus);
+};
+
+deleteMessageError = function(data, textStatus, jqXHR) {
+	console.error("error deleteMessage. textStatus = "+textStatus);
+	searchByNumberCallback(JSON.stringify(data));
+};
+
+searchByNumberSuccess = function(data, textStatus, jqXHR) {
+	console.log("success searchByNumber. textStatus = "+textStatus);
+	var returnData = {};
+	returnData.textStatus = textStatus;
+	searchByNumberCallback(JSON.stringify(returnData));
+	
+};
+
+
+searchByNumberError = function(data, textStatus, jqXHR) {
+	console.log("error searchByNumber. textStatus = "+textStatus);
 };
 
 getUrl = function(requestedPath) {
@@ -60,7 +85,7 @@ message.sendMessage = function(recipient, text) {
 };
 
 message.getMessages = function(callback) {
-	sendMessageCallback = callback;
+	getMessagesCallback = callback;
 	$.ajax({
 		type : 'GET',
 		url : getUrl(),
@@ -69,12 +94,32 @@ message.getMessages = function(callback) {
 	});
 };
 
-message.getMessage = function(messageId) {
+message.getMessage = function(messageId, callback) {
+	getMessagesCallback = callback;
 	$.ajax({
 		type : 'GET',
 		url : getUrl(messageId),
 		success : getMessagesSuccess,
 		error : getMessagesError
+	});
+}
+
+message.deleteMessage = function(messageId) {
+	$.ajax({
+		type : 'DELETE',
+		url : getUrl(messageId),
+		success : deleteMessageSuccess,
+		error : deleteMessageError
+	});
+}
+
+message.searchByNumber = function(number, callback) {
+	searchByNumberCallback = callback;
+	$.ajax({
+		type : 'GET',
+		url : getUrl("filter/"+number),
+		success : searchByNumberSuccess,
+		error : searchByNumberError
 	});
 }
 
