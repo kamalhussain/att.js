ee337f5ff76de206541373cac308a6bb0254a104
Lance Stout <lancestout@gmail.com>
2013-04-22 08:49:30 -0700

    Simplify phone plugin.

--- src/att.phone.phono.js
+++ src/att.phone.phono.js
@@ -1,5 +1,55 @@
 (function (ATT, $) {
 
+    function AttCall(att, call) {
+        var self = this
+
+        self.att = att;
+        self._call = call;
+
+        WildEmitter.call(this);
+
+        self.on('*', function (eventType) {
+            self.att.emit(eventType, self);
+        });
+
+        return self;
+    }
+
+    AttCall.prototype = Object.create(WildEmitter.prototype, {
+        constructor: {
+            value: AttCall
+        }
+    });
+
+    AttCall.prototype.answer = function () {
+        return this._call.answer();
+    };
+
+    AttCall.prototype.hangup = function () {
+        return this._call.hangup();
+    };
+
+    AttCall.prototype.__defineGetter__("initiator", function () { 
+        return this._call.initiator; 
+    });
+
+    ATT.fn.Call = AttCall;
+
+
+    ATT.fn.dial = function (number) {
+        var self = this,
+            callable = this.phoneNumber.getCallable(number);
+
+        self.emit('calling', number);
+
+        var call = self._makeCall(callable);
+
+        call.emit('ring');
+        self.emit('outgoingCall', call);
+
+        return call;
+    };
+
     ATT.fn._makeCall = function (phoneNumber) {
         var self = this;
 
