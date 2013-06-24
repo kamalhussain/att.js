47914fc0042bbf2c9520aa440235630a450bf6af
Thao Nguyen <boulethao@gmail.com>
2013-06-24 16:26:51 -0700

    Added the "calling" event to the unit tests

--- testing/tests/wcg.phonecancelring.js
+++ testing/tests/wcg.phonecancelring.js
@@ -7,7 +7,7 @@ test("Basic init and event tests for WCG plugin: cancel a call after a ringing e
     var nb_to_dial = '1-804-222-1111';
     //N.B.: Before running this test, make sure that index.html page is located under http://localhost/testing.
     //If not, change the client_id and the redirect_uri accordingly.
-    //expect(10);
+    expect(11);
 
     att = new ATT({
         clientID: client_id,
@@ -35,7 +35,7 @@ test("Basic init and event tests for WCG plugin: cancel a call after a ringing e
 
         att.on("phoneReady", function () {
             //check if user has been registered
-            ok("phoneReady", "Phone is ready");
+            ok("phoneReady", "phoneReady: Phone is ready");
             ok(att.wcgBackend.wcgService._sessionID, "User has a session id: registered");
             //make a call to a test number that automatically reads your phone number and ends the call: callBegin should be raised.
             att.dial(nb_to_dial);
@@ -43,8 +43,13 @@ test("Basic init and event tests for WCG plugin: cancel a call after a ringing e
         });
 
         att.on("ring", function (call) {
-            ok("ring", "Phone has rung");
+            ok("ring", "ring: Phone has rung");
             call.hangup();
+        });
+
+        att.on("calling", function (dialedNumber) {
+            ok("calling", "calling: calling " + dialedNumber);
+            equal(ATT.phoneNumber.parse(nb_to_dial), dialedNumber, "calling: Dialed number: " + dialedNumber);
         });
 
         att.on("outgoingCall", function (call) {
@@ -56,28 +61,28 @@ test("Basic init and event tests for WCG plugin: cancel a call after a ringing e
 
 
         att.on("callBegin", function (call) {
-            ok("callBegin", "Call has begun");
+            ok("callBegin", "callBegin: Call has begun");
         });
 
         att.on("callEnd", function (call) {
-            ok("callEnd", "Call has ended");
+            ok("callEnd", "callEnd: Call has ended");
             att.logout();
         });
 
         att.on("phoneClose", function () {
-            ok("phoneClose", "Phone is close");
+            ok("phoneClose", "phoneClose: Phone is close");
             //check if user has logged out
             equal(att.wcgBackend.wcgService, null, "User has logged out");
             start();
         });
 
         att.on("callError", function (call) {
-            ok(false, "this should not happen");
+            ok(false, "callError: this should not happen");
 
         });
 
         att.on("phoneError", function () {
-            ok(false, "this should not happen");
+            ok(false, "phoneError: this should not happen");
 
         });
 
--- testing/tests/wcg.phonehangup.js
+++ testing/tests/wcg.phonehangup.js
@@ -7,7 +7,7 @@ test("Basic init and event tests for WCG plugin: hangup a phone call", function 
     var nb_to_dial = '1-804-222-1111';
     //N.B.: Before running this test, make sure that index.html page is located under http://localhost/testing.
     //If not, change the client_id and the redirect_uri accordingly.
-    expect(10);
+    expect(12);
 
     att = new ATT({
         clientID: client_id,
@@ -35,7 +35,7 @@ test("Basic init and event tests for WCG plugin: hangup a phone call", function 
 
         att.on("phoneReady", function () {
             //check if user has been registered
-            ok("phoneReady", "Phone is ready");
+            ok("phoneReady", "phoneReady: Phone is ready");
             ok(att.wcgBackend.wcgService._sessionID, "User has a session id: registered");
             //make a call to a test number that automatically reads your phone number and ends the call: callBegin should be raised.
             att.dial(nb_to_dial);
@@ -43,11 +43,16 @@ test("Basic init and event tests for WCG plugin: hangup a phone call", function 
         });
 
         att.on("ring", function (call) {
-            ok("ring", "Phone has rung");
+            ok("ring", "ring: Phone has rung");
+        });
+
+        att.on("calling", function (dialedNumber) {
+            ok("calling", "calling: calling " + dialedNumber);
+            equal(ATT.phoneNumber.parse(nb_to_dial), dialedNumber, "calling: Dialed number: " + dialedNumber);
         });
 
         att.on("outgoingCall", function (call) {
-            ok("outgoingCall", "Number has been dialed");
+            ok("outgoingCall", "outgoingCall: Number has been dialed");
             var numberToMatch = call.remotePeer.match(/sip\:([^@]*)@/);
             var number = numberToMatch ? numberToMatch[1] : null;
             equal(number, ATT.phoneNumber.parse(nb_to_dial), "Dialed number: " + number);
@@ -56,31 +61,31 @@ test("Basic init and event tests for WCG plugin: hangup a phone call", function 
 
 
         att.on("callBegin", function (call) {
-            ok("callBegin", "Call has begun");
+            ok("callBegin", "callBegin: Call has begun");
             setTimeout(function() {
                 call.hangup();
             }, 3000);
         });
 
         att.on("callEnd", function (call) {
-            ok("callEnd", "Call has ended");
+            ok("callEnd", "callEnd: Call has ended");
             att.logout();
         });
 
         att.on("phoneClose", function () {
             ok("phoneClose", "Phone is close");
             //check if user has logged out
-            equal(att.wcgBackend.wcgService, null, "User has logged out");
+            equal(att.wcgBackend.wcgService, null, "phoneClose: User has logged out");
             start();
         });
 
         att.on("callError", function (call) {
-            ok(false, "this should not happen");
+            ok(false, "callError: this should not happen");
 
         });
 
         att.on("phoneError", function () {
-            ok(false, "this should not happen");
+            ok(false, "phoneError: this should not happen");
 
         });
 
