b2492d210b16fc4e8f4d8f380a3084e0dc5ae8dc
fairchild <fairchild@stimble.net>
2013-06-26 02:49:31 -0700

    WIP, not working yet, attempt to use a layout for jade templates html

--- scripts/renderHtml.js
+++ scripts/renderHtml.js
@@ -2,7 +2,13 @@ var jade = require('jade'),
     fs = require('fs');
 
 module.exports = function (spec, template) {
+    var layoutFile = fs.readFileSync('scripts/layout.jade', 'utf8');
+    var layoutFunc = jade.compile(layoutFile, { pretty: true, filename: 'scripts/layout.jade' });
+    
     var templateFile = fs.readFileSync('scripts/' + template + '.jade', 'utf-8'),
         templateFunc = jade.compile(templateFile, {pretty: true});
-    return templateFunc(spec);
+    return  layoutFunc(templateFunc(spec), spec)
+        
+    // return templateFunc(spec);
 };
+
