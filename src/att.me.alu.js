21b97022b20c24d7ae291ef55aeb702090e8ac67
Kamal Hussain <kamalhusain@yahoo.com>
2013-07-01 15:45:46 -0500

    initPlugin changes

--- src/att.me.alu.js
+++ src/att.me.alu.js
@@ -1,19 +1,3 @@
-/**
- *  Copyright (c) 2013 Alcatel-Lucent
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 (function(ATT, $) {
     var cache = {};
 
@@ -92,7 +76,7 @@
                     (typeof att.config.settings.token.key === 'undefined')) {
                 att.getMe(function(me) {
                     // make it possible to override guessed version
-                    me.version = att.config.version || _.getQueryParam('version') || me.version;
+                    me.version = att.config.version || ATT._.getQueryParam('version') || me.version;
                     att.config.version = me.version;
                     att.config.myNumber = me.number;
 
